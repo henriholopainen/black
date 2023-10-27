66770e55c898e070f8ff8a12dffbcf4da3f47d75
Henri Holopainen <henri.i.holopainen@gmail.com>
2023-10-27 23:02:38 +0300

    Add test case

---
+++ tests/data/cases/fmtlines.py
@@ -0,0 +1,24 @@
+# flags: --lines 6 6
+def foo () :
+
+
+
+    if True  :
+        a  = 123
+
+
+        b =  666
+
+        print( "Foobar")
+
+# output
+def foo () :
+
+
+
+    if True  :
+        a = 123
+
+        b =  666
+
+        print( "Foobar")
