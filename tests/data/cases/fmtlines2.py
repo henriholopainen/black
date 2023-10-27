402d7e5314295b2d6f6bfa6768bf2cd4ed09c398
Henri Holopainen <henri.i.holopainen@gmail.com>
2023-10-27 23:43:05 +0300

    Another test case

---
+++ tests/data/cases/fmtlines2.py
@@ -0,0 +1,21 @@
+# flags: --lines 1 11
+def foo () :
+
+
+
+    if True  :
+        a  = 123
+
+
+        b =  666
+
+        print( "Foobar")
+
+# output
+def foo():
+    if True:
+        a = 123
+
+        b = 666
+
+        print("Foobar")
